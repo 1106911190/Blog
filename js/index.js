df35c0da4a0c235d7c90872c6cb1a9141a41aefa
Yang Wenqiang <1106911190@qq.com>
2015-12-02 23:28:28 +0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+	$('.slide_body .task_list .task_title').click(function(){
+		if($(this).nextAll('li').css('display')=='none'){
+			$(this).nextAll('li').slideDown(400);
+		}else{
+			$(this).nextAll('li').slideUp(500);
+		}
+	})
+	$('.close').click(function(){
+		$('.slide').children().slideUp('fast');
+		$('.slide').animate({
+			'width':'0px'
+		},'slow');
+		$('body').animate({
+			'padding-left': '0px'},
+			'slow', function() {
+			$('.open').show();
+		});
+		$('.close').hide();
+	})
+	$('.open').click(function(){
+		$('body').animate({
+			'padding-left': '20%'},
+			'slow', function() {
+			$('.close').show();
+		});
+		$('.slide').animate({
+			'width':'20%'
+		},'slow',function(){
+		});
+		$('.slide').children().slideDown('fast');
+		$('.open').hide();
+		
+	})	
+})
